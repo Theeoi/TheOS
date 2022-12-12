c156db0af9ccc70ac37665a79f908a5837b64e27
Theodor Blom <me@theodorblom.com>
2022-12-12 19:46:25 +0100

    feat(scripts): JACK audio interface startup

---
+++ scripts/start_jack.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Startup script for initializing JACK - a high-end audio interface
+# This is needed for audio production on linux machines.
+# Setup to run on startup if of interest.
+
+jack_control start
+jack_control ds alsa
+jack_control dps device hw:iTwo
+jack_control dps rate 48000
+jack_control dps nperiods 3
+jack_control dps period 64
+sleep 10
+qjackctl &
