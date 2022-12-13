2e08cffe6359f43cbaef063e59041b08ac16a2ca
Theodor Blom <me@theodorblom.com>
2022-12-13 23:10:32 +0100

    fix: kwriteconfig5 is not a package

--- linuxscoop-antdark/install.sh
+++ linuxscoop-antdark/install.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
   exit 2
 fi
 
-req_packages="git svn unzip kwriteconfig5"
+req_packages="git svn unzip"
 pacman_check=$(pacman -Q $req_packages 2>&1)
 if [[ $pacman_check = *"was not found"* ]]; then
   echo "You do not have all required packages to run $0."
